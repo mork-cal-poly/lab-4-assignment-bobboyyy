a32d4140d97cb77913a0d9b373cd440f2e1b1848
bobboyyy <147883245+bobboyyy@users.noreply.github.com>
2023-10-23 22:34:12 -0700

    added the stovetop background, and vase, along with some things to start animating and check for a click

--- src/sketch.js
+++ src/sketch.js
@@ -7,14 +7,28 @@ function setup() {
   myCanvas.parent("canvas-parent");
 }
 
+let flowerX = 0;
+let flowerY = 0;
+
+let slothX = 0;
+let slothY = 0;
+
 function draw() {
-  background(220);
-  drawSloth(100, 300);
+  background("#404040");
+  push();
+  translate(200, 320);
+  strokeWeight(0);
+  pan();
+  pop();
+
+  drawSloth(80, 350);
+
+  drawFlowers(200, 150);
 }
 
 function drawSloth(x, y) {
   push();
-  translate(x, y);
+  translate(x + slothX, y + slothY);
   //push();
   //translate(200, 400);
   fill("#6D7347");
@@ -62,3 +76,99 @@ function drawSloth(x, y) {
   //pop();
   pop();
 }
+
+function pan() {
+  //stovetop
+  fill("#212121");
+  rect(-130, -320, 400, 400);
+  stroke("white");
+  strokeWeight(1);
+  circle(0, 50, 170);
+  circle(230, -160, 170);
+  circle(230, 50, 170);
+
+  //pan handle
+  strokeWeight(0);
+  fill("grey");
+  stroke("grey");
+  strokeWeight(30);
+  line(-200, -160, 0, -160);
+
+  //spatula
+
+  //handle
+  stroke("brown");
+  strokeWeight(10);
+  line(-80, -290, 10, -290);
+
+  //spatula
+  stroke("rgb(80,80,80)");
+  triangle(30, -290, 40, -305, 40, -275);
+
+  //other part of handle
+  strokeWeight(0);
+  fill("grey");
+  rect(10, -295, 18, 10);
+
+  //other part of spatula
+  stroke("grey");
+  rect(45, -320, 60, 61);
+
+  //pan
+  strokeWeight(0);
+  ellipse(0, -160, 180);
+  fill("#575757");
+  ellipse(0, -160, 165);
+}
+
+function drawFlowers(x, y) {
+  push();
+  translate(x + flowerX, y + flowerY);
+  //flowers
+  strokeWeight(5);
+  stroke("green");
+  line(-5, -120, -33, -160);
+  line(5, -120, 30, -150);
+  line(0, -120, 0, -180);
+
+  //pedals
+  strokeWeight(0);
+  fill("yellow");
+  //left
+  ellipse(-45, -160, 25);
+  ellipse(-30, -170, 25);
+  ellipse(-30, -150, 25);
+  ellipse(-23, -160, 25);
+
+  //middle
+  ellipse(-7, -183, 25);
+  ellipse(5, -190, 25);
+  ellipse(10, -180, 25);
+  ellipse(3, -170, 22);
+
+  //right
+  ellipse(20, -150, 20);
+  ellipse(30, -160, 25);
+  ellipse(40, -150, 25);
+  ellipse(33, -140, 22);
+
+  //head of flower
+  fill("brown");
+  ellipse(-33, -160, 17);
+  ellipse(30, -150, 17);
+  ellipse(0, -180, 17);
+  strokeWeight(0);
+
+  //bottom of vase
+  fill("#5F5D9C");
+  ellipse(0, 0, 90, 200);
+
+  //top of vase
+  strokeWeight(0);
+  fill("#5F5D9C");
+  triangle(0, -60, -45, -125, 45, -125);
+
+  pop();
+}
+
+function mouseClicked() {}
